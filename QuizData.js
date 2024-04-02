31984b8ec67754887099bab01086d52782c55acf
trustgone <trustgone@azurecomputer.com>
2024-04-02 17:23:33 +0700

    ReactNative-QuizApp Commit Code

---
+++ QuizData.js
@@ -0,0 +1,202 @@
+export default data = [
+  {
+    question: "What should you do when approaching a yellow traffic light?",
+    options: [
+      "Speed up and cross the intersection quickly",
+      "Come to a complete stop",
+      "Slow down and prepare to stop",
+      "Ignore the light and continue driving",
+    ],
+    correct_option: "Slow down and prepare to stop",
+  },
+  {
+    question: "What does a red octagonal sign indicate?",
+    options: [
+      "Yield right of way",
+      "Stop and proceed when safe",
+      "Merge with traffic",
+      "No left turn allowed",
+    ],
+    correct_option: "Stop and proceed when safe",
+  },
+  {
+    question: "What is the purpose of a crosswalk?",
+    options: [
+      "A designated area for parking",
+      "A place to stop and rest",
+      "A path for pedestrians to cross the road",
+      "A location for U-turns",
+    ],
+    correct_option: "A path for pedestrians to cross the road",
+  },
+  {
+    question: "What is the capital city of Australia?",
+    options: [
+      "Sydney",
+      "Melbourne",
+      "Canberra",
+      "Brisbane",
+    ],
+    correct_option: "Canberra",
+  },
+  {
+    question: "What is the chemical symbol for Gold?",
+    options: [
+      "Gd",
+      "Go",
+      "Ag",
+      "Au",
+    ],
+    correct_option: "Au",
+  },
+  {
+    question: "In what year was the first iPhone released?",
+    options: [
+      "2005",
+      "2007",
+      "2008",
+      "2010",
+    ],
+    correct_option: "2007",
+  },
+  {
+    question: "What is the tallest mountain in the world?",
+    options: [
+      "K2",
+      "Mount Everest",
+      "Mount Kilimanjaro",
+      "Denali",
+    ],
+    correct_option: "Mount Everest",
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    options: [
+      "Leonardo da Vinci",
+      "Michelangelo",
+      "Raphael",
+      "Caravaggio",
+    ],
+    correct_option: "Leonardo da Vinci",
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    options: [
+      "Venus",
+      "Mars",
+      "Jupiter",
+      "Saturn",
+    ],
+    correct_option: "Mars",
+  },
+  {
+    question: "Who discovered electricity?",
+    options: [
+      "Isaac Newton",
+      "Nikola Tesla",
+      "Michael Faraday",
+      "Benjamin Franklin",
+    ],
+    correct_option: "Benjamin Franklin",
+  },
+  {
+    question: "What is the world's largest ocean?",
+    options: [
+      "Atlantic Ocean",
+      "Indian Ocean",
+      "Pacific Ocean",
+      "Southern Ocean",
+    ],
+    correct_option: "Pacific Ocean",
+  },
+  {
+    question: "Who came up with the theory of relativity?",
+    options: [
+      "Edgar Allan Poe",
+      "Albert Einstein",
+      "Galileo Galilei",
+      "Louis Pasteur",
+    ],
+    correct_option: "Albert Einstein",
+  },
+  {
+    question: "What is the national bird of the United States?",
+    options: [
+      "Eagle",
+      "Bald Eagle",
+      "Condor",
+      "Pigeon",
+    ],
+    correct_option: "Eagle",
+  },
+  {
+    question: "What language is spoken in Brazil?",
+    options: [
+      "Spanish",
+      "Portuguese",
+      "English",
+      "French",
+    ],
+    correct_option: "Portuguese",
+  },
+  {
+    question: "What is the world's largest ocean?Who directed the movie Jurassic Park?",
+    options: [
+      "Steven Spielberg",
+      "George Lucas",
+      "Michael Bay",
+      "Stanley Kubrick",
+    ],
+    correct_option: "Steven Spielberg",
+  },
+  {
+    question: "What is sushi traditionally wrapped in?",
+    options: [
+      "Rice Paper",
+      "Seaweed",
+      "Bamboo",
+      "Lettuce",
+    ],
+    correct_option: "Seaweed",
+  },
+  {
+    question: "What is the main ingredient in hummus?",
+    options: [
+      "Potatoes",
+      "Lentils",
+      "Chickpeas",
+      "White Beans",
+    ],
+    correct_option: "Chickpeas",
+  },
+  {
+    question: "Who is the author of the Harry Potter series?",
+    options: [
+      "J.D. Salinger",
+      "Roald Dahl",
+      "Suzanne Collins",
+      "J.K. Rowling",
+    ],
+    correct_option: "J.K. Rowling",
+  },
+  {
+    question: "How many players are there in a soccer team?",
+    options: [
+      "6",
+      "11",
+      "9",
+      "4",
+    ],
+    correct_option: "11",
+  },
+  {
+    question: "What does a barometer measure?",
+    options: [
+      "Sound",
+      "Light",
+      "Atmospheric Pressure",
+      "Humidity",
+    ],
+    correct_option: "Atmospheric Pressure",
+  },
+];
--- README.md
+++ README.md
@@ -1,17 +1,41 @@
 # ReactNative-QuizApp
 
-Expo
-•	expo
-•	expo-status-bar
+Simple and easy to follow guide to start a Quiz App which uses the Navigation and Animation. It includes Routes, Animation, Screens
 
-React-Native
-•	@react-navigation/native
-•	@react-navigation/native-stack
-•	react-native-screens
-•	react-native-safe-area-context
+## Screenshots
 
-Run the app
-•	git clone https://github.com/medesigndd/ReactNative-QuizApp.git
-•	cd ReactNative-QuizApp
-•	yarn install
-•	yarn run android
+<span style="padding-right: 50px">
+<img src="/docs/img1.png" height="400" width="200" />
+<img src="/docs/img2.png" height="400" width="200" />
+<img src="/docs/img3.png " height="400" width="200" />
+<img src="/docs/img4.png" height="400" width="200" />
+<img src="/docs/img5.png" height="400" width="200" />
+<img src="/docs/img6.png" height="400" width="200" />
+</span>
+
+## Screens
+
+- Welcome Screen
+- QuizPage Screen
+- Questions Screen
+- ProgressBar Screen
+- Result Screen
+
+## Expo
+
+- expo
+- expo-status-bar
+
+## React-Native
+
+- @react-navigation/native
+- @react-navigation/native-stack
+- react-native-screens
+- react-native-safe-area-context
+
+## Run the app
+
+- git clone https://github.com/n-bhasin/ReactNative-QuizApp.git
+- cd ReactNative-QuizApp
+- yarn install
+- yarn run ios
